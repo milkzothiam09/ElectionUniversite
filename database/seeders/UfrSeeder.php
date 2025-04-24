fc01cbbb4e4429e3ee678dbde943e2978197de34
milkzothiam09 <thiampapemalick2002@gmail.com>
2025-04-24 22:23:29 +0000

    Premier  commit

---
+++ database/seeders/UfrSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class UfrSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+    }
+}
