3c5925619b8f0f05feb56251071e2a0e4783eab6
milkzothiam09 <thiampapemalick2002@gmail.com>
2025-04-27 00:42:07 +0000

    Ajout de nouvelles modifications

--- database/seeders/UniversiteSeeder.php
+++ database/seeders/UniversiteSeeder.php
@@ -16,5 +16,43 @@ class UniversiteSeeder extends Seeder
             'city' => 'Lomé',
             'country' => 'Togo'
         ]);
+
+        Universite::create([
+            'name' => 'Université de Thiès',
+            'acronym' => 'UT',
+            'city' => 'Thiès',
+            'country' => 'Sénégal'
+        ]);
+
+        Universite::create([
+            'name' => 'Université Cheikh Anta Diop',
+            'acronym' => 'UCAD',
+            'city' => 'Dakar',
+            'country' => 'Sénégal'
+        ]);
+        Universite::create([
+            'name' => 'Université de Ouagadougou',
+            'acronym' => 'UO',
+            'city' => 'Ouagadougou',
+            'country' => 'Burkina Faso'
+        ]);
+        Universite::create([
+            'name' => 'Université de Kara',
+            'acronym' => 'UK',
+            'city' => 'Kara',
+            'country' => 'Togo'
+        ]);
+        Universite::create([
+            'name' => 'Université de Ziguinchor',
+            'acronym' => 'UZ',
+            'city' => 'Ziguinchor',
+            'country' => 'Sénégal'
+        ]);
+        Universite::create([
+            'name' => 'Université de Bambey',
+            'acronym' => 'UB',
+            'city' => 'Bambey',
+            'country' => 'Sénégal'
+        ]);
     }
 }
